7afe2de678f5e1a142315d8e4d674fd763c8816d
Andrew Kyeyune <43235557+Andrew-Kyeyune@users.noreply.github.com>
2025-04-24 13:17:02 +0300

    Add project files.

---
+++ Feedback_Form/Controllers/FormController.cs
@@ -0,0 +1,37 @@
+﻿using Feedback_Form.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Feedback_Form.Controllers
+{
+    public class FormController : Controller
+    {
+        private static List<Feedback> Feedbacks = new List<Feedback>
+        {
+            new Feedback {Customer_ID = 1001, Name = "Andrew", Email_Id = "ctdigital18@gmail.com", Mobile_No = "256772591881" }
+        };
+
+        public IActionResult Index()
+        {
+            return View(Feedbacks); // Corrected reference
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Feedback feedback)
+        {
+            if (ModelState.IsValid)
+            {
+                feedback.Customer_ID = Feedbacks.Max(b => b.Customer_ID) + 1; //Sample Id generation
+                Feedbacks.Add(feedback);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(Feedbacks);
+
+        }
+    }
+}
---
+++ Feedback_Form/Models/Feedback.cs
@@ -0,0 +1,12 @@
+﻿namespace Feedback_Form.Models
+{
+    public class Feedback
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Email_Id { get; set; }
+        //public string Email_ID { get; internal set; }
+        public required string Mobile_No { get; set; }
+        public int Customer_ID { get; internal set; }
+    }
+}
